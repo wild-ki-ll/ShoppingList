a29a35813054845996a8de81a5e718007b67bc64
maria <maria@eldis.ru>
2017-01-24 12:41:09 +0700

    added item form submode

--- src/main/scala/shopping-list/ItemsMode.scala
+++ src/main/scala/shopping-list/ItemsMode.scala
@@ -4,28 +4,81 @@
 
 package ShoppingList
 
-import japgolly.scalajs.react.{BackendScope, ReactComponentB, Callback}
-import japgolly.scalajs.react.vdom.prefix_<^._
+import japgolly.scalajs.react.{BackendScope, Callback, ReactComponentB}
+import japgolly.scalajs.react.vdom.prefix_<^.{<, ^, _}
 
 object ItemsMode {
-  class Backend($: BackendScope[Unit, List[Item]]) {
+
+  case class ItemsModeState(
+    idSubmode: Int
+  )
+
+  val customStyle = "customStyle".reactStyle
+
+  class ItemBackend($: BackendScope[Unit, Item]) {
+    def render(i: Item) =
+      <.div(
+        <.div(
+          <.label("Наименование"),
+          <.input(^.`type` := "text"),
+          <.select(
+            <.option("Продукты питания"),
+            <.option("Бытовая химия"),
+            <.option("Канцелярия"),
+            <.option("Парфюмерия и косметика"),
+            <.option("Книги"),
+            <.option("Прочее")
+          ),
+          <.select(
+            <.option("Молочные продукты"),
+            <.option("Крупы"),
+            <.option("Хлебо-булочные изделия"),
+            <.option("Мясо"),
+            <.option("Полуфабрикаты"),
+            <.option("Прочее")
+          )
+        ),
+        <.div(
+          <.label("Количество"),
+          <.input(^.`type` := "number"),
+          <.select(
+            <.option("шт"),
+            <.option("кг"),
+            <.option("л"),
+            <.option("г")
+          ),
+          <.label(^.float := "none", <.input.checkbox(), "Важное!")
+        )
+      )
+  }
+
+  val сreateItemForm = ReactComponentB[Unit]("ItemSubmode")
+    .initialState(Item(name = "", company = 0, unit = 0, quantity = 0, importance = false))
+    .renderBackend[ItemBackend]
+    .build
+
+  val createItemSubmode = <.div(сreateItemForm())
+
+  class Backend($: BackendScope[Unit, ItemsModeState]) {
+
+
     def createMenu =
       <.menu(
-        <.button("Добавить", ^.onClick --> Callback.alert("Добавить в режиме покупок")),
-        <.button("Редактировать"),
-        <.button("Удалить"),
-        <.button("Фильтр")
+        <.button("Добавить",      ^.onClick --> $.modState(_.copy(idSubmode = 1))),
+        <.button("Редактировать", ^.onClick --> $.modState(_.copy(idSubmode = 1))),
+        <.button("Удалить",       ^.onClick --> $.modState(_.copy(idSubmode = 0))),
+        <.button("Фильтр",        ^.onClick --> $.modState(_.copy(idSubmode = 0)))
       )
 
-    def render(s: List[Item]) =
+    def render(s: ItemsModeState) =
       <.div (
         createMenu(),
-        <.div ("Режим покупок")
+        if (s.idSubmode == 1) createItemSubmode() else <.div ("Режим покупок")
       )
   }
 
   val createMode = ReactComponentB[Unit]("ItemMode")
-    .initialState(List[Item]())
+    .initialState(ItemsModeState(idSubmode = 0))
     .renderBackend[Backend]
     .build
 
