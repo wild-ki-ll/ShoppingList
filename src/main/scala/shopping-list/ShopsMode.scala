aac6493e4154ad799332bf8d77df84d7973ae13b
kirill <kirill@eldis.ru>
2017-01-24 15:35:56 +0700

    add ModeType enumeration

--- src/main/scala/shopping-list/ShopsMode.scala
+++ src/main/scala/shopping-list/ShopsMode.scala
@@ -9,16 +9,23 @@ import japgolly.scalajs.react.vdom.prefix_<^._
 
 object ShopsMode {
 
-  case class StateShopMode(isList: Boolean = true, name: String = "", addr: String = "", list: List[Shop] = List[Shop]())
+  object ModeType extends Enumeration {
+    type ModeType = Value
+    val listMode, itemMode, filterMode = Value
+  }
+  import ModeType._
+
+  case class StateShopMode(modeType: ModeType = listMode, name: String = "", addr: String = "", list: List[Shop] = List[Shop]())
 
   class Backend($: BackendScope[Unit, StateShopMode]) {
 
     // change state
-    def showItemData()= $.modState(s => s.copy(isList = false))
-    def showList()    = $.modState(s => s.copy(isList = true))
+    def showItemData()= $.modState(s => s.copy(modeType = itemMode))
+    def showList()    = $.modState(s => s.copy(modeType = listMode))
+    def showFilter()  = $.modState(s => s.copy(modeType = filterMode))
     def deleteShop()  = $.modState(s => s)
     def filterShop()  = $.modState(s => s)
-    def addShop()     = $.modState(s => s.copy(isList = true, name="", addr="", list = s.list :+ Shop(s.name, 0, s.addr)))
+    def addShop()     = $.modState(s => s.copy(modeType = itemMode, name="", addr="", list = s.list :+ Shop(s.name, 0, s.addr)))
 
     def onChangeName(e: ReactEventI) = {
       val newVal = e.target.value
@@ -36,7 +43,7 @@ object ShopsMode {
         <.button("Добавить",      ^.onClick --> showItemData()),
         <.button("Редактировать", ^.onClick --> showItemData()),
         <.button("Удалить",       ^.onClick --> deleteShop()),
-        <.button("Фильтр",        ^.onClick --> filterShop())
+        <.button("Фильтр",        ^.onClick --> showFilter())
       )
 
     def createItem(sh: Shop) = {
@@ -76,10 +83,19 @@ object ShopsMode {
       )
     }
 
+    def createFilter = {<.div("Тут будет фильтр")}
+
+    def createDataMode(s: StateShopMode) =
+      s.modeType match {
+        case ModeType.itemMode    => createItem(Shop(s.name, 0, s.addr))
+        case ModeType.filterMode  => createFilter()
+        case _                    => createTable(s.list)
+      }
+
     def render(s: StateShopMode) = {
       <.div(
         createMenu(),
-        if (s.isList) createTable(s.list) else createItem(Shop(s.name, 0, s.addr))
+        createDataMode(s)
       )
     }
   }
